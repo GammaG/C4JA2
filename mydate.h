fa72d5ad5abdd6af3461e72fc469c7b1920ed2f1
Marco <m_seidler@hotmail.de>
2013-05-03 14:31:13 +0200

    updates

--- mydate.h
+++ mydate.h
@@ -2,30 +2,52 @@
 #define MYDATE_H
 
 
+
 namespace MyDate  {
 
 
 class Days{
-    unsigned int m_day = 0;
+    unsigned int m_day;
 public:
-    Days();
+
+    Days(unsigned int i = 0) :m_day(i){ }
+
+
     unsigned int value() const {return m_day; }
+
+    friend bool operator== (const unsigned int& i,const Days& lhs){
+
+    return i == lhs.value();
+
+    }
+
+
 };
 
 
 class Months{
-    unsigned int m_month = 0;
+    unsigned int m_month;
 public:
-    Months();
+    Months(unsigned int i = 0) :m_month(i){ }
     unsigned int value() const {return m_month; }
+    friend bool operator== (const unsigned int& i,const Months& lhs){
+
+    return i == lhs.value();
+
+    }
 
 };
 
 class Years {
-    unsigned int m_year = 0;
+    unsigned int m_year;
 public:
-    Years();
+    Years(unsigned int i = 0) :m_year(i){ }
     unsigned int value() const {return m_year; }
+    friend bool operator== (const unsigned int& i,const Years& lhs){
+
+    return i == lhs.value();
+
+    }
 
 };
 
