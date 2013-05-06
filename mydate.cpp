5fbbab017e528aad097468812ec5685ae5607fe2
Marco <m_seidler@hotmail.de>
2013-05-06 14:55:26 +0200

    update

--- mydate.cpp
+++ mydate.cpp
@@ -1,1 +1,47 @@
+#include "mydate.h"
 
+/**
+ * Wert des Tages wird zurückgegeben.
+ * @brief MyDate::Days::value
+ * @return
+ */
+unsigned int MyDate::Days::value() const {
+    return MyDate::Days::m_day;
+}
+
+/**
+ *Gibt den Monat zurück
+ * @brief value
+ * @return
+ */
+unsigned int MyDate::Months::value() const {return m_month; }
+
+
+/**
+ *Gibt den Monat zurück
+ * @brief value
+ * @return
+ */
+unsigned int MyDate::Years::value() const {return m_year; }
+
+
+/**
+ *Gibt den Tag des Datums zurück
+  * @brief MyDate::Date::day
+  * @return
+  */
+ MyDate::Days MyDate::Date::day() const {return days;}
+
+ /**
+  *Gibt den Monat des Datums zurück
+  * @brief MyDate::Date::month
+  * @return
+  */
+ MyDate::Months MyDate::Date::month() const { return months; }
+
+
+ /** Gibt das Jahr des Datums zurück
+ * @brief MyDate::Date::year
+ * @return
+ */
+MyDate::Years MyDate::Date::year() const { return years; }
--- mydate.h
+++ mydate.h
@@ -12,16 +12,13 @@ public:
 
     Days(unsigned int i = 0) :m_day(i){ }
 
+    unsigned int value() const;
 
-    unsigned int value() const {return m_day; }
+    friend bool operator== (const unsigned int& i,const Days& rhs){
 
-    friend bool operator== (const unsigned int& i,const Days& lhs){
+        return i == rhs.value();
 
-    return i == lhs.value();
-
-    }
-
-
+        }
 };
 
 
@@ -29,7 +26,7 @@ class Months{
     unsigned int m_month;
 public:
     Months(unsigned int i = 0) :m_month(i){ }
-    unsigned int value() const {return m_month; }
+    unsigned int value() const;
     friend bool operator== (const unsigned int& i,const Months& lhs){
 
     return i == lhs.value();
@@ -42,7 +39,7 @@ class Years {
     unsigned int m_year;
 public:
     Years(unsigned int i = 0) :m_year(i){ }
-    unsigned int value() const {return m_year; }
+    unsigned int value() const;
     friend bool operator== (const unsigned int& i,const Years& lhs){
 
     return i == lhs.value();
@@ -56,11 +53,15 @@ class Date {
     Months months;
     Years years;
 public:
-    Date();
-    Days day() const {return days;}
-    Months month() const { return months; }
-    Years year() const { return years; }
+    Date(Days days = 0, Months months = 0, Years years = 0)
+        :days(days),
+        months(months),
+        years(years)
+    {}
 
+    Days day() const;
+    Months month() const;
+    Years year() const;
 
 };
 
