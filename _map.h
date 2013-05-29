3eb9a01a09890e8dcbd81f8899110988870f38fe
Admin <administrator@ubuntu.(none)>
2013-05-29 20:12:35 +0200

    bla

---
+++ _map.h
@@ -0,0 +1,319 @@
+#include "mydate.h"
+#include "map.h"
+
+/**
+ * @author Admin
+ * @date 27.05.13
+ */
+
+namespace templateSpace {
+
+/** Hier wird die Größe des aktuellen Baumen zurückgegeben
+ * @brief Map<KeyT,T>::getSize
+ * @return
+ */
+
+template<class KeyT, class T>
+size_t templateSpace::Map<KeyT,T>::getSize() const{
+
+    return m_size;
+
+}
+
+
+/**
+ *Methode Clont einen Knoten gibt den neuen Konten zurück.
+ * @brief Map<KeyT,T>::Node::clone
+ * @param parent
+ * @return
+ */
+template<class KeyT, class T>
+typename Map<KeyT,T>::Node* Map<KeyT,T>::Node::clone(Map<KeyT,T>::Node* parent){
+    Map<KeyT,T>::Node* n = new Map<KeyT,T>::Node(m_pair.first,m_pair.second,parent);
+
+    if(m_left != 0){
+        n->m_left = m_left->clone(n);
+
+    } else{
+        n->m_left = 0;
+    }
+
+    if(m_right != 0){
+        n->m_right = m_right->clone(n);
+
+    } else{
+        n->m_right = 0;
+    }
+
+return n;
+}
+
+/**
+ * Operatorüberladung, setzt die Message der aktuellen Node
+ */
+template<class KeyT, class T>
+void Map<KeyT,T>::Node::operator= (Map<KeyT,T>::mapped_t mapped){
+    this->m_pair.second = mapped;
+}
+
+
+
+/** Gibt den Root knoten der Map zurück
+ * @brief Map<KeyT,T>::getRootNode
+ * @return
+ */
+template<class KeyT, class T>
+typename Map<KeyT,T>::Node* Map<KeyT,T>::getRootNode(){
+     return Map<KeyT,T>::m_root;
+ }
+
+/** Setzt den Wert zu einem Key neu, konveriert einen übergebenen Int zu gültigem String
+ * @brief Map<KeyT,T>::setMessage
+ * @param key
+ * @param nr
+ */
+template<class KeyT, class T>
+void Map<KeyT,T>::setMessage(Map<KeyT,T>::key_t key ,Map<KeyT,T>::mapped_t str){
+    Node& n = find(*m_root,key);
+    n.m_pair.second = str;
+}
+
+template<class KeyT, class T>
+bool Map<KeyT,T>::Iterator::operator== (Map<KeyT,T>::Iterator& rhs){
+    return this->m_root==rhs.m_root;
+}
+
+template<class KeyT, class T>
+bool Map<KeyT,T>::Iterator::operator!= (Map<KeyT,T>::Iterator& rhs){
+    return this->m_root!=rhs.m_root;
+}
+
+
+/** Operatorüberladung Methode sucht einen Node der zum gegebenen Key passt,
+  * existiert dieser nicht, wird er neu angelegt und dann zurück gegeben.
+  * @brief Map<KeyT,T>::operator []
+  * @param key
+  * @return
+  */
+template<class KeyT, class T>
+ typename Map<KeyT,T>::mapped_t& Map<KeyT,T>::operator[](const Map<KeyT,T>::key_t& key){
+
+     if(this->getRootNode()==0){
+
+         m_root = new Map<KeyT,T>::Node(key, mapped_t() ,0);
+
+         this->m_size++;
+         return m_root->m_pair.second;
+
+      }
+        else{
+         return find(*m_root,key).m_pair.second;
+     }
+
+
+ }
+
+ /** Methode erhöht den Map internen Counter und gibt den aktuellen Wert zurück
+   * @brief Map<KeyT,T>::increaseCounter
+   * @return
+   */
+ template<class KeyT, class T>
+  int Map<KeyT,T>::increaseCounter(){
+    counter++;
+    return counter;
+
+  }
+
+  /** Operatorüberladung, die Methode überschreibt die = Methode und bewirkt
+  * das eine übergebene Map neu erzeugt und alle Werte kopiert werden
+  * @brief Map<KeyT,T>::operator =
+  * @param rhs
+  */
+ template<class KeyT, class T>
+ void Map<KeyT,T>::operator= (Map& rhs){
+     Map();
+     this->m_root = rhs.m_root->clone(0);
+     this->counter = rhs.counter;
+     this->m_size = rhs.m_size;
+
+ }
+
+
+
+ /** Find Methode, sucht rekursiv nach dem übergebenen Key den Suchbaum durch,
+  * existiert dieser nicht wird er angelegt und dann returnd
+  * @brief Map<KeyT,T>::find
+  * @param last - beim ersten Aufruf sollte der Root übergeben werden, danach wird in der
+  * Rekursion der Nächste Node übergeben
+  * @param key
+  * @return Node bei dem der Key matched
+  */
+ template<class KeyT, class T>
+ typename Map<KeyT,T>::Node& Map<KeyT,T>::find(Map<KeyT,T>::Node& last,const Map<KeyT,T>::key_t& key){
+     if(last.m_pair.first==key){
+         //last.setMessage(convertInt(increaseCounter()));
+         return last;
+     }
+     else if(last.m_pair.first<key){
+         if(last.getRightNode()==0){
+
+             last.setRightNode(*last.insert(key,last));
+             m_size++;
+             return *last.getRightNode();
+
+         }  else{
+            return Map<KeyT,T>::find(*last.getRightNode(),key);
+            }
+     } else {
+         if(last.getLeftNode()==0){
+             last.setLeftNode(*last.insert(key,last));
+             m_size++;
+             return *last.getLeftNode();
+         }
+         return find(*last.getLeftNode(),key);
+     }
+
+
+ }
+
+ /** Sucht rekursiv den ganzen Suchbaum durch, wird der Node nicht gefunden wird eine constante
+  * ohne Inhalt zurückgegeben.
+  * @brief Map<KeyT,T>::findReadOnly
+  * @param last
+  * @param key
+  * @return
+  */
+ template<class KeyT, class T>
+ const typename Map<KeyT,T>::mapped_t& Map<KeyT,T>::findReadOnly(Map<KeyT,T>::Node& last,const Map<KeyT,T>::key_t& key)const {
+     if(last.m_pair.first==key){
+         return last.m_pair.second;
+     }
+     else if(last.m_pair.first<key){
+         if(last.getRightNode()==0){
+            return Map<KeyT,T>::M_NOT_IN_MAP;
+
+         }  else{
+            return Map<KeyT,T>::findReadOnly(*last.getRightNode(),key);
+            }
+     } else {
+         if(last.getLeftNode()==0){
+             return Map<KeyT,T>::M_NOT_IN_MAP;
+         }
+         return findReadOnly(*last.getLeftNode(),key);
+     }
+
+
+ }
+
+ /** Operatorüberladung zum suchen eines Wertes zu einem gegebenen Key,
+  * ohne schreibrechte ( es wird kein neuer Node erzeugt )
+  * @brief Map<KeyT,T>::operator []
+  * @param key
+  * @return
+  */
+ template<class KeyT, class T>
+ const typename Map<KeyT,T>::mapped_t& Map<KeyT,T>::operator[](const key_t key) const{
+     return findReadOnly(*m_root,key);
+
+ }
+
+
+ /** Methode prüft in einem Suchbaum ob ein Wert vorhanden ist
+  * @brief Map<KeyT,T>::contains
+  * @param last - Sollte zum Beginn der Root Node sein, wird danach
+  * von der Rekursion neu gesetzt
+  * @param key
+  * @return
+  */
+ template<class KeyT, class T>
+ bool Map<KeyT,T>::contains(Map<KeyT,T>::Node& last, const Map<KeyT,T>::key_t& key){
+     if(last.m_pair.first==key){
+         return true;
+     } else if(last.m_pair.first<key){
+         if(last.getRightNode()==0){
+             return false;
+         }
+         return contains(*last.getRightNode(),key);
+     } else {
+         if(last.getLeftNode()==0){
+             return false;
+         }
+         return contains(*last.getLeftNode(),key);
+     }
+     return false;
+
+ }
+
+
+ /** Methode erzeugt einen Neuen Node und gibt ihn zurück
+  * @brief Map<KeyT,T>::Node::insert
+  * @param key
+  * @param mapped
+  * @param upNode
+  * @return
+  */
+ template<class KeyT, class T>
+ typename Map<KeyT,T>::Node* Map<KeyT,T>::Node::insert(const Map<KeyT,T>::key_t& key, Map<KeyT,T>::Node& upNode){
+
+     Map<KeyT,T>::Node *n = new Map<KeyT,T>::Node(key, mapped_t(),&upNode);
+
+    return n;
+}
+
+ /** Methode überschreibt den UpNode des aktuellen Nodes
+  * @brief Map<KeyT,T>::Node::setUpNode
+  * @param x
+  */
+ template<class KeyT, class T>
+ void Map<KeyT,T>::Node::setUpNode(Node& x){
+    this->m_up = &x;
+}
+
+ /** Methode überschreibt den LeftNode des aktuellen Nodes
+ * @brief Map<KeyT,T>::Node::setLeftNode
+ * @param x
+ */
+template<class KeyT, class T>
+void Map<KeyT,T>::Node::setLeftNode(Node& x){
+    this->m_left = &x;
+}
+
+/** Methode überschreibt den RigthNode des aktuellen Nodes
+ * @brief Map<KeyT,T>::Node::setRightNode
+ * @param x
+ */
+template<class KeyT, class T>
+void Map<KeyT,T>::Node::setRightNode(Node& x){
+    this->m_right = &x;
+}
+
+
+/** Methode gibt den UpNode des aktuellen Nodes zurück
+ * @brief Map<KeyT,T>::Node::getUpNode
+ * @return
+ */
+template<class KeyT, class T>
+typename Map<KeyT,T>::Node* Map<KeyT,T>::Node::getUpNode(){
+    return this->m_up;
+}
+
+/** Methode gibt den LeftNode des aktuellen Nodes zurück
+ * @brief Map<KeyT,T>::Node::getLeftNode
+ * @return
+ */
+template<class KeyT, class T>
+typename Map<KeyT,T>::Node* Map<KeyT,T>::Node::getLeftNode(){
+    return this->m_left;
+}
+
+/** Methode gibt den RightNode des aktuellen Nodes zurück
+ * @brief Map<KeyT,T>::Node::getRightNode
+ * @return
+ */
+template<class KeyT, class T>
+typename Map<KeyT,T>::Node* Map<KeyT,T>::Node::getRightNode(){
+    return this->m_right;
+}
+
+
+}
---
+++ map.h
@@ -0,0 +1,114 @@
+#ifndef MAP_H
+#define MAP_H
+
+/**
+ * @author Admin
+ * @date 27.05.13
+ */
+
+namespace templateSpace {
+
+/** Deklarartion der Klasse Map, diese beinhaltet die Klassendefinition der Node,
+ * die Suchfunktionen, sowie den root Node und die Anzahl
+ * @brief The Map class
+ */
+template<class KeyT, class T> class Map{
+public:
+    typedef KeyT key_t;
+    typedef T mapped_t;
+
+
+    /** Klasse Node behinhaltet ein Wertepaar auf einem Key und einem Wert
+     * @brief The Node class
+     */
+    class Node{
+
+    public:
+        std::pair<key_t,mapped_t> m_pair; // key/value
+        Node *m_up, *m_left, *m_right; //pointers to other nodes
+
+        explicit Node(const key_t key, const mapped_t mapped, Node* parent):
+            m_pair(key,mapped), m_up(parent), m_left(0), m_right(0) {}
+
+
+        explicit Node(Node* clone):
+            m_pair(clone->m_pair.first,clone->m_pair.second), m_up(clone->getUpNode()), m_left(clone->getLeftNode()), m_right(clone->getRightNode()) {}
+
+
+        void setUpNode(Node& x);
+        void setLeftNode(Node& x);
+        void setRightNode(Node& x);
+        Node* getUpNode();
+        Node* getLeftNode();
+        Node* getRightNode();
+        Node* clone(Node* clone);
+        void operator= (mapped_t mapped);
+
+
+
+        Node* insert(const key_t& key, Node& upNode);
+
+        ~Node(){
+            if(m_left != 0){
+            delete(m_left);
+            }
+            if(m_left != 0){
+            delete(m_right);
+            }
+
+        }
+
+
+        };
+
+    class Iterator{
+    public:
+
+        Node* m_root;
+        explicit Iterator(const Node* node = 0 ): m_root(node){}
+        bool operator== (Iterator& node);
+        bool operator!= (Iterator& node);
+        * std::pair operator* (&Map map);
+
+    };
+
+    Node* m_root; //root node of the search tree
+    size_t m_size; // number of elements in tree
+    const mapped_t M_NOT_IN_MAP;  //return val if not in map
+
+    int counter;
+
+    Map():m_root(0),counter(0),m_size(0){}
+
+    size_t getSize() const;
+
+    Node* getRootNode();
+
+    void setMessage(key_t key ,mapped_t str);
+
+
+    bool contains(Node& last, const key_t& key);
+
+    mapped_t& operator[](const key_t& key);
+
+    const mapped_t& operator[](const key_t key) const;
+
+    Node& find(Node& last , const key_t& key);
+    const mapped_t& findReadOnly(Node& last, const key_t& key)const;
+
+    std::string convertInt(int number);
+
+    int increaseCounter();
+
+    void operator= (Map& map);
+
+    ~Map() {
+        if(m_root != 0){
+        delete(m_root);
+        }}
+
+    };
+}
+
+#include "_map.h"
+#endif // MAP_H
